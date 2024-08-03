791d3042f9c1c17746eda23b3079a5c129ba162f
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-03 17:45:35 +0500

    Create Models/AzureBlobResponse.cs : StatusCode, ReasonPhrase, BlobUri, IsError, FileName, FileBytes, ErrorMessage; Create Services/BlobStorage/IAzureBlopbhStorage.cs : TAsk<AzureBlobResponse> UploadFileAsync(string blobContainer, string directoryName, string fileName, string fileStream) method;

---
+++ UploadFileAzureBlobStorage/Models/AzureBlobResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace UploadFileAzureBlobStorage.Models
+{
+    public class AzureBlobResponse
+    {
+        public int StatusCode { get; set; }
+        public string ReasonPhrase { get; set; }
+        public Uri BlobUri { get; set; }
+        public bool IsError { get; set; }
+        public string FileName { get; set; }
+        public byte[]? FileBytes { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
---
+++ UploadFileAzureBlobStorage/Services/BlobStorage/IAzureBlogService.cs
@@ -0,0 +1,10 @@
+﻿using UploadFileAzureBlobStorage.Models;
+
+namespace UploadFileAzureBlobStorage.Services.BlobStorage
+{
+    public interface IAzureBlogService
+    {
+        Task<AzureBlobResponse> UploadFileAsync(string blobContainer, 
+            string directoryName, string fileName, string fileStream);
+    }
+}
