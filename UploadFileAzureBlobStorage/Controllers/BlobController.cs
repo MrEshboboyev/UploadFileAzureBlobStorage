f2191e98a02213d8b6c51388100c9026b3ecd90e
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-03 18:45:10 +0500

    Create BlobController.cs : DI for IAzureBlobService.cs; Service lifetime added Program.cs for I(AzureBlobService);

---
+++ UploadFileAzureBlobStorage/Controllers/BlobController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UploadFileAzureBlobStorage.Services.BlobStorage;
+
+namespace UploadFileAzureBlobStorage.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlobController : ControllerBase
+    {
+        private readonly IAzureBlobService _azureBlobService;
+
+        public BlobController(IAzureBlobService azureBlobService)
+        {
+            _azureBlobService = azureBlobService;
+        }
+    }
+}
--- UploadFileAzureBlobStorage/Program.cs
+++ UploadFileAzureBlobStorage/Program.cs
@@ -1,3 +1,5 @@
+using UploadFileAzureBlobStorage.Services.BlobStorage;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// adding lifetime for IAzureBlobService
+builder.Services.AddScoped<IAzureBlobService, AzureBlobService>();
 
 var app = builder.Build();
 
